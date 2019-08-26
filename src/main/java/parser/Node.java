5c0b6e6dbf5f03ed4ba929d5769a94c84a3d0b38
RomanMorozov88 <MorozovRoman.88@mail.ru>
2019-08-26 07:26:34 +0300

    ParserBracesFirst

---
+++ src/main/java/parser/Node.java
@@ -0,0 +1,20 @@
+package parser;
+
+public class Node {
+
+    private int open;
+    private int close;
+
+    public Node(int open, int close) {
+        this.open = open;
+        this.close = close;
+    }
+
+    public int getOpen() {
+        return open;
+    }
+
+    public int getClose() {
+        return close;
+    }
+}
